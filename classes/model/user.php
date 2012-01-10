ee47012ec16b1cecb76ce99c3289c4a8b26fdf57
Lillem4n <lilleman@larvit.se>
2012-01-10 09:47:33 +0100

    Fixed password hash so it actually works ;)

--- classes/model/user.php
+++ classes/model/user.php
@@ -311,7 +311,7 @@ class Model_User extends Model
 	 */
 	public function login_by_username_and_password($username, $password)
 	{
-		if ($user_id = self::driver()->get_user_id_by_username_and_password($username, self::password_encrypt($password)))
+		if ($user_id = self::driver()->get_user_id_by_username_and_password($username, self::password_encrypt($password, $username)))
 		{
 			if ($this->instance_name) $_SESSION['modules']['pajas'][$this->instance_name] = $user_id;
 			return $this->load_user_data($user_id);
@@ -378,7 +378,7 @@ class Model_User extends Model
 
 		if ( ! self::username_available($username)) return FALSE;
 
-		$user_id = self::driver()->new_user($username, self::password_encrypt($password), $user_data);
+		$user_id = self::driver()->new_user($username, self::password_encrypt($password, $username), $user_data);
 
 		if ($load_to_instance)
 		{
@@ -400,14 +400,12 @@ class Model_User extends Model
 	 * Encrypt a password
 	 *
 	 * @param str $password - plain text password
+	 * @param str $username - username of the user that gets their password encrypted
 	 * @return string - encrypted
 	 */
-	public static function password_encrypt($password)
+	public static function password_encrypt($password, $username)
 	{
-		$salt = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
-		$hash = hash('sha256', $salt.$password);
-
-		return $salt.$hash;
+		return hash_hmac('sha256', $username.$password.Kohana::$config->load('user.password_salt'), Kohana::$config->load('user.shared_key'));
 	}
 
 	/**
@@ -458,7 +456,7 @@ class Model_User extends Model
 
 			if (isset($user_data['password']))
 			{
-				self::driver()->set_password($this->get_user_id(), self::password_encrypt($user_data['password']));
+				self::driver()->set_password($this->get_user_id(), self::password_encrypt($user_data['password'], $this->get_username()));
 				unset($user_data['password']);
 			}
 
--- config/user.php
+++ config/user.php
@@ -10,4 +10,6 @@ return array(
 //	'driver' => 'mysql',
 	'driver'        => Kohana::$config->load('pdo.default.driver'),
 	'root_password' => 'toor', // Set to FALSE to inactivate root account
+	'password_salt' => 'change this to something random',
+	'shared_key'    => 'this should also be something random',
 );
